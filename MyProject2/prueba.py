153b76913e41fa4e3de9b97a361d0a7845425bc4
jorafael3 <jalvaradoe3@gmail.com>
2024-05-27 00:09:32 -0500

    sd

--- MyProject2/prueba.py
+++ MyProject2/prueba.py
@@ -38,18 +38,18 @@ def run_csharp_file_with_parameter(parameter):
         
 def Guardar_encrypt(cedula_encr,cedula):
         try:
-            conexion = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="",
-                database="crediweb"
-            )
             # conexion = mysql.connector.connect(
-            #     host="50.87.184.179",
-            #     user="wsoqajmy_jorge",
-            #     password="Equilivre3*",
-            #     database="wsoqajmy_crediweb"
+            #     host="localhost",
+            #     user="root",
+            #     password="",
+            #     database="crediweb"
             # )
+            conexion = mysql.connector.connect(
+                host="50.87.184.179",
+                user="wsoqajmy_jorge",
+                password="Equilivre3*",
+                database="wsoqajmy_crediweb"
+            )
 
             cursor = conexion.cursor()
             consulta = """
@@ -73,18 +73,18 @@ def Cargar_Datos():
         
         try:
             time.sleep(0.5)
-            conexion = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="",
-                database="crediweb"
-            )
             # conexion = mysql.connector.connect(
-            #     host="50.87.184.179",
-            #     user="wsoqajmy_jorge",
-            #     password="Equilivre3*",
-            #     database="wsoqajmy_crediweb"
+            #     host="localhost",
+            #     user="root",
+            #     password="",
+            #     database="crediweb"
             # )
+            conexion = mysql.connector.connect(
+                host="50.87.184.179",
+                user="wsoqajmy_jorge",
+                password="Equilivre3*",
+                database="wsoqajmy_crediweb"
+            )
 
             
             if conexion.is_connected():
